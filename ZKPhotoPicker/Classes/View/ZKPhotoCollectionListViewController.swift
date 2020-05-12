c06d13f190baaddaa50d5106c7eb72fee622baaa
Doby's MBP <403317854@qq.com>
2020-05-12 23:07:02 +0800

    fix：修复大图查看滑动超出collectionview的可视范围后返回崩溃的bug

--- ZKPhotoPicker/Classes/View/ZKPhotoCollectionListViewController.swift
+++ ZKPhotoPicker/Classes/View/ZKPhotoCollectionListViewController.swift
@@ -95,16 +95,22 @@ extension ZKPhotoCollectionListViewController: ZKPhotoShowDataSourceAndDelegate 
     }
     
     func cellFrameAt(index: Int) -> CGRect {
-        let cell = self.collectionView.cellForItem(at: .init(item: index, section: 0))!
-        return self.view.convert(cell.frame, from: self.collectionView)
+        //根据layout拿到动画cell的frame
+        let cellFrame = self.collectionView.layoutAttributesForItem(at: .init(item: index, section: 0))!.frame
+        //转换到self.view的坐标系
+        return self.view.convert(cellFrame, from: self.collectionView)
     }
     
     func pageVC(_ pageVC: ZKPhotoShowPageViewController, didScroll2Index idx: Int) {
-        if let topIndexPath = self.collectionView.indexPathsForVisibleItems.first, idx < topIndexPath.item {
+        //这个visibleItems里木有顺序，所以要先排下序，比较最大最小值
+        let sortedIndexPaths = self.collectionView.indexPathsForVisibleItems.sorted(by: {$0<$1})
+        //如果超出顶部，移动collectionview到顶
+        if let topIndexPath = sortedIndexPaths.first, idx < topIndexPath.item {
             self.collectionView.scrollToItem(at: .init(item: idx, section: 0), at: .top, animated: false)
         }
-        else if let bottomIndexPath = self.collectionView.indexPathsForVisibleItems.last, idx > bottomIndexPath.item {
-            self.collectionView.scrollToItem(at: .init(item: idx, section: 0), at: .bottom, animated: false)
+        //超出底部，移动collectionview到底
+        else if let bottomIndexPath = sortedIndexPaths.last, idx > bottomIndexPath.item {
+            self.collectionView.scrollToItem(at: .init(item: idx, section: 0), at: .bottom, animated: true)
         }
     }
     
